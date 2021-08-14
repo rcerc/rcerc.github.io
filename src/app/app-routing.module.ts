824b1363f100bbf0529adc6e7b8cc381f410331f
rcerc <88944439+rcerc@users.noreply.github.com>
2021-08-14 16:15:13 -0400

    Set up routing to posts/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/posts.module').then((m) => m.PostsModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'posts',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { PostsRoutingModule } from './posts-routing.module';
+
+@NgModule({
+  imports: [PostsRoutingModule],
+})
+export class PostsModule {}
